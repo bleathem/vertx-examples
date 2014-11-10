237f9f5e6e5670379b8aa83a05bf674164c46ef9
Julien Viet <julien@julienviet.com>
2014-11-10 11:30:03 +0100

    Extracted the gigantic anonymous inner class in a class

--- src/main/java/io/vertx/examples/ConvertingProcessor.java
+++ src/main/java/io/vertx/examples/ConvertingProcessor.java
@@ -1,36 +1,12 @@
 package io.vertx.examples;
 
-import com.sun.source.tree.AssignmentTree;
-import com.sun.source.tree.BinaryTree;
-import com.sun.source.tree.BlockTree;
-import com.sun.source.tree.ConditionalExpressionTree;
-import com.sun.source.tree.EnhancedForLoopTree;
-import com.sun.source.tree.ExpressionStatementTree;
-import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.ForLoopTree;
-import com.sun.source.tree.IdentifierTree;
-import com.sun.source.tree.IfTree;
-import com.sun.source.tree.LambdaExpressionTree;
-import com.sun.source.tree.LiteralTree;
-import com.sun.source.tree.MemberSelectTree;
-import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.NewClassTree;
-import com.sun.source.tree.ParenthesizedTree;
-import com.sun.source.tree.StatementTree;
-import com.sun.source.tree.Tree;
-import com.sun.source.tree.UnaryTree;
-import com.sun.source.tree.VariableTree;
 import com.sun.source.util.TreePath;
-import com.sun.source.util.TreePathScanner;
 import com.sun.source.util.Trees;
 import com.sun.tools.javac.code.Flags;
-import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.comp.Attr;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.util.Context;
-import io.vertx.codegen.ClassKind;
 import io.vertx.codegen.TypeInfo;
 import io.vertx.core.AbstractVerticle;
 import org.junit.Assert;
@@ -63,7 +39,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
@@ -155,243 +130,10 @@ public class ConvertingProcessor extends AbstractProcessor {
             if (exeElt.getSimpleName().toString().equals("start") && exeElt.getParameters().isEmpty()) {
               attributeClass(rootElt);
               TreePath path = trees.getPath(exeElt);
-              TreePathScanner<CodeModel, VisitContext> visitor = new TreePathScanner<CodeModel, VisitContext>() {
-
-                public StatementModel scan(StatementTree tree, VisitContext visitContext) {
-                  return (StatementModel) scan((Tree) tree, visitContext);
-                }
-
-                public ExpressionModel scan(ExpressionTree tree, VisitContext visitContext) {
-                  return (ExpressionModel) scan((Tree) tree, visitContext);
-                }
-
-                @Override
-                public CodeModel visitForLoop(ForLoopTree node, VisitContext p) {
-                  if (node.getInitializer().size() != 1) {
-                    throw new UnsupportedOperationException();
-                  }
-                  if (node.getUpdate().size() != 1) {
-                    throw new UnsupportedOperationException();
-                  }
-                  StatementModel initializer = scan(node.getInitializer().get(0), p);
-                  ExpressionModel update = scan(node.getUpdate().get(0).getExpression(), p);
-                  StatementModel body = scan(node.getStatement(), p);
-                  ExpressionModel condition = scan(node.getCondition(), p);
-                  return lang.forLoop(initializer, condition, update, body);
-                }
-
-                @Override
-                public CodeModel visitEnhancedForLoop(EnhancedForLoopTree node, VisitContext p) {
-                  ExpressionModel expression = scan(node.getExpression(), p);
-                  StatementModel body = scan(node.getStatement(), p);
-                  return lang.enhancedForLoop(node.getVariable().getName().toString(), expression, body);
-                }
-
-                @Override
-                public CodeModel visitAssignment(AssignmentTree node, VisitContext context) {
-                  ExpressionModel variable = scan(node.getVariable(), context);
-                  ExpressionModel expression = scan(node.getExpression(), context);
-                  return ExpressionModel.forAssign(variable, expression);
-                }
-
-                @Override
-                public StatementModel visitVariable(VariableTree node, VisitContext p) {
-                  JCTree.JCVariableDecl decl = (JCTree.JCVariableDecl) node;
-                  ExpressionModel initializer;
-                  if (node.getInitializer() != null) {
-                    initializer = scan(node.getInitializer(), p);
-                  } else {
-                    initializer = null;
-                  }
-                  TypeInfo type = factory.create(decl.type);
-                  return lang.variable(
-                      type,
-                      decl.name.toString(),
-                      initializer
-                  );
-                }
-
-                @Override
-                public StatementModel visitIf(IfTree node, VisitContext visitContext) {
-                  ExpressionModel condition = scan(node.getCondition(), visitContext);
-                  StatementModel thenBody = scan(node.getThenStatement(), visitContext);
-                  StatementModel elseBody = node.getElseStatement() != null ? scan(node.getElseStatement(), visitContext) : null;
-                  return StatementModel.ifThenElse(condition, thenBody, elseBody);
-                }
-
-                @Override
-                public CodeModel visitConditionalExpression(ConditionalExpressionTree node, VisitContext visitContext) {
-                  ExpressionModel condition = scan(node.getCondition(), visitContext);
-                  ExpressionModel trueExpression = scan(node.getTrueExpression(), visitContext);
-                  ExpressionModel falseExpression = scan(node.getFalseExpression(), visitContext);
-                  return ExpressionModel.forConditionalExpression(condition, trueExpression, falseExpression);
-                }
-
-                @Override
-                public ExpressionModel visitUnary(UnaryTree node, VisitContext p) {
-                  ExpressionModel expression = scan(node.getExpression(), p);
-                  switch (node.getKind()) {
-                    case POSTFIX_INCREMENT:
-                      return expression.onPostFixIncrement();
-                    default:
-                      throw new UnsupportedOperationException();
-                  }
-                }
-
-                @Override
-                public CodeModel visitExpressionStatement(ExpressionStatementTree node, VisitContext context) {
-                  ExpressionModel expression = scan(node.getExpression(), context);
-                  return StatementModel.render(renderer -> expression.render(renderer));
-                }
-
-                @Override
-                public ExpressionModel visitBinary(BinaryTree node, VisitContext p) {
-                  ExpressionModel left = scan(node.getLeftOperand(), p);
-                  ExpressionModel right = scan(node.getRightOperand(), p);
-                  String op;
-                  switch (node.getKind()) {
-                    case PLUS:
-                      op = "+";
-                      break;
-                    case LESS_THAN:
-                      op = "<";
-                      break;
-                    case MULTIPLY:
-                      op = "*";
-                      break;
-                    default:
-                      throw new UnsupportedOperationException();
-                  }
-                  return lang.combine(left, op, right);
-                }
-
-                @Override
-                public ExpressionModel visitLiteral(LiteralTree node, VisitContext p) {
-                  switch (node.getKind()) {
-                    case STRING_LITERAL:
-                      return lang.stringLiteral(node.getValue().toString());
-                    case BOOLEAN_LITERAL:
-                      return ExpressionModel.render(renderer -> renderer.getLang().renderBooleanLiteral(node.getValue().toString(), renderer));
-                    case INT_LITERAL:
-                      return ExpressionModel.render(renderer -> renderer.getLang().renderIntegerLiteral(node.getValue().toString(), renderer));
-                    default:
-                      throw new UnsupportedOperationException();
-                  }
-                }
-
-                @Override
-                public ExpressionModel visitIdentifier(IdentifierTree node, VisitContext context) {
-                  JCTree.JCIdent ident = (JCTree.JCIdent) node;
-                  if (ident.sym instanceof TypeElement) {
-                    if (ident.type.equals(SystemType)) {
-                      return ExpressionModel.forMemberSelect("out", () ->
-                          ExpressionModel.forMemberSelect("println", () ->
-                              ExpressionModel.forMethodInvocation(args -> lang.console(args.get(0)))));
-                    } else {
-                      TypeInfo.Class type = (TypeInfo.Class) factory.create(ident.type);
-                      if (type.getKind() == ClassKind.API) {
-                        return ExpressionModel.forMemberSelect((identifier) -> lang.staticFactory(type, identifier));
-                      } else if (type.getKind() == ClassKind.JSON_OBJECT) {
-                        return ExpressionModel.forNew(args -> {
-                          switch (args.size()) {
-                            case 0:
-                              return lang.jsonObject();
-                            default:
-                              throw new UnsupportedOperationException();
-                          }
-                        });
-                      } else if (type.getKind() == ClassKind.JSON_ARRAY) {
-                        return ExpressionModel.forNew(args -> {
-                          switch (args.size()) {
-                            case 0:
-                              return lang.jsonArray();
-                            default:
-                              throw new UnsupportedOperationException();
-                          }
-                        });
-                      } else if (type.getKind() == ClassKind.OPTIONS) {
-                        return ExpressionModel.forNew(args -> lang.options(type));
-                      } else {
-                        return lang.classExpression(type);
-                      }
-                    }
-                  } else {
-                    ExpressionModel alias = context.getAlias(ident.sym);
-                    if (alias != null) {
-                      return alias;
-                    } else {
-                      return ExpressionModel.render(node.getName().toString());
-                    }
-                  }
-                }
-
-                @Override
-                public CodeModel visitNewClass(NewClassTree node, VisitContext visitContext) {
-                  ExpressionModel identifier = scan(node.getIdentifier(), visitContext);
-                  List<ExpressionModel> arguments = node.getArguments().stream().map(arg -> scan(arg, visitContext)).collect(Collectors.toList());
-                  return identifier.onNew(arguments);
-                }
-
-                @Override
-                public CodeModel visitParenthesized(ParenthesizedTree node, VisitContext visitContext) {
-                  ExpressionModel expression = scan(node.getExpression(), visitContext);
-                  return ExpressionModel.forParenthesized(expression);
-                }
-
-                @Override
-                public ExpressionModel visitMemberSelect(MemberSelectTree node, VisitContext p) {
-                  ExpressionModel expression = scan(node.getExpression(), p);
-                  return expression.onMemberSelect(node.getIdentifier().toString());
-                }
-
-                @Override
-                public ExpressionModel visitMethodInvocation(MethodInvocationTree node, VisitContext p) {
-                  ExpressionModel methodSelect = scan(node.getMethodSelect(), p);
-                  List<ExpressionModel> arguments = node.getArguments().stream().map(argument -> scan(argument, p)).collect(Collectors.toList());
-                  return methodSelect.onMethodInvocation(arguments);
-                }
-
-                @Override
-                public StatementModel visitBlock(BlockTree node, VisitContext p) {
-                  List<StatementModel> statements = node.getStatements().stream().map((statement) -> scan(statement, p)).collect(Collectors.toList());
-                  return StatementModel.block(statements);
-                }
-
-                @Override
-                public ExpressionModel visitLambdaExpression(LambdaExpressionTree node, VisitContext p) {
-                  List<String> parameterNames = node.getParameters().stream().map(parameter -> parameter.getName().toString()).collect(Collectors.toList());
-                  List<TypeInfo> parameterTypes = node.getParameters().stream().
-                      map(parameter -> factory.create(((JCTree.JCVariableDecl) parameter).type)).
-                      collect(Collectors.toList());
-                  int size = parameterNames.size();
-                  if (size > 0) {
-                    JCTree.JCVariableDecl last = (JCTree.JCVariableDecl) node.getParameters().get(size - 1);
-                    if (last.vartype instanceof JCTree.JCTypeApply) {
-                      JCTree.JCTypeApply typeApply = (JCTree.JCTypeApply) last.vartype;
-                      if (typeApply.clazz instanceof JCTree.JCFieldAccess) {
-                        JCTree.JCFieldAccess clazz = (JCTree.JCFieldAccess) typeApply.clazz;
-                        Symbol.ClassSymbol sym = (Symbol.ClassSymbol) clazz.sym;
-                        TypeInfo type = factory.create(sym.type);
-                        if (type.getKind() == ClassKind.ASYNC_RESULT) {
-                          ExpressionModel result = lang.asyncResult(last.name.toString());
-                          CodeModel body = scan(node.getBody(), p.putAlias(last.sym, result));
-                          return lang.asyncResultHandler(node.getBodyKind(), last.name.toString(), body);
-                        }
-                      }
-                    }
-                  }
-                  CodeModel body = scan(node.getBody(), p);
-                  return lang.lambda(node.getBodyKind(), parameterTypes, parameterNames, body);
-                }
-
-                @Override
-                public CodeModel visitMethod(MethodTree node, VisitContext p) {
-                  return scan(node.getBody(), p);
-                }
-              };
-              CodeModel src = visitor.scan(path, new VisitContext());
+              ModelBuilder builder = new ModelBuilder(SystemType, factory, lang);
+              CodeModel model = builder.build(path);
               CodeWriter writer = new CodeWriter(lang);
-              src.render(writer);
+              model.render(writer);
               result.put(rootElt.toString().replace('.', '/') + '.' + lang.getExtension(), writer.getBuffer().toString());
             }
           }
